be7e6032a58f5e10efc66446cee446da185f3340
Jesper <jesper.kuiper99@gmail.com>
2019-11-18 15:38:22 +0100

    Data toegevoegd

--- AfvalOphaler/AfvalOphaler/Program.cs
+++ AfvalOphaler/AfvalOphaler/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AfvalOphaler
 {
     class Program
     {
+        const string distanceDir = @"..\..\data\dist.txt";
+        const string ordersDir = @"..\..\data\order.txt";
+
         static void Main(string[] args)
         {
+            string[] text = File.ReadAllLines(ordersDir);
+            Console.WriteLine(text[0]);
+            Console.ReadKey();
         }
     }
 }
