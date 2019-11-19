18674f5e0b5f566511de573dedbed38f90bbb27f
Jesper <jesper.kuiper99@gmail.com>
2019-11-19 18:53:09 +0100

    distances veranderd, distparser toegevoegd

--- AfvalOphaler/AfvalOphaler/Parser.cs
+++ AfvalOphaler/AfvalOphaler/Parser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AfvalOphaler
 {
@@ -11,5 +12,22 @@ namespace AfvalOphaler
         // Spaties weghalen
         // Splitsen op ;
         // DoMagic(overgebleven_dingen)
+
+        public static void ParseDistances(string dir, int nIds, out int[,] dists, out int[,] times)
+        {
+            string[] lines = File.ReadAllLines(dir);
+            dists = times = new int[nIds+1, nIds+1];
+            for(int i = 1; i < lines.Length; i++)
+            {
+                string[] row = lines[i].Split(';');
+                int[] rowd = new int[row.Length];
+                for(int j = 0; j < row.Length; j++)
+                {
+                    rowd[j] = int.Parse(row[j]);
+                }
+                dists[rowd[0], rowd[1]] = rowd[2];
+                times[rowd[0], rowd[1]] = rowd[3];
+            }
+        }
     }
 }
